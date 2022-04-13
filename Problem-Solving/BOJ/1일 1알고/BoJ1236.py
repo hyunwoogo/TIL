419bc1dbc198cccfe07a514240f6715e8c741bc2
hyunwoogo <cksl1220@naver.com>
2022-04-13 21:20:41 +0900

    update PS

---
+++ Problem-Solving/BOJ/1일 1알고/BoJ1236.py
@@ -0,0 +1,24 @@
+row, col = map(int, input().split())
+castle = []
+count_r = 0
+count_c = 0
+lst = []
+
+for i in range(row):
+    security = input()
+    security_lst = [security[j] for j in range(col)]
+    castle.append(security_lst)
+
+for k in range(row):
+    if "X" not in castle[k]:
+        count_r += 1
+
+for _ in range(col):
+    for n in range(row):
+        lst.append(castle[n][_])
+    if "X" not in lst:
+        count_c += 1
+
+answer = max(count_r, count_c)
+
+print(answer)
